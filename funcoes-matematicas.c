013cb9f7db5b0c6f40d1df41b8350aad97ef1795
KPMGE <jesuskevin254@gmail.com>
2020-06-27 13:00:28 -0300

    adicionando comentarios ao codigo

--- funcoes-matematicas.c
+++ funcoes-matematicas.c
@@ -4,13 +4,16 @@
 float calculaBaskara(int a, int b, int c){
     float delta, raiz1, raiz2;
 
+    //calculando o valor de delta
     delta = pow(b, 2) - 4*a*c;
 
+    //caso delta seja menor que 0, nao ha raizes reis
     if(delta < 0){
         printf("nao ha raizes reais!!");
         return 0;
     }
 
+    //do contrario, calculamos as raizes
     else{
         raiz1 = (-b + sqrt(delta))/2*a;
         raiz2 = (-b - sqrt(delta))/2*a;
@@ -23,15 +26,20 @@ float calculaBaskara(int a, int b, int c){
 }
 
 int verificaPrimo(int num){
+    //variaveis declaradas
     int primo=0, i=2;
+
+    //dividindo o numero por todos os numeros menores que ele começando por 2
     while(i < num){
         if(num%i == 0){
+            //caso divisivel por algum numero , variavel primo incrementada
             primo++;
         }
-
+        //incremento a variavel i
         i++; 
     }
 
+    //caso nao divisivel por nenhum dos numeros, primo
     if(primo == 0){
         printf("o numero e primo!");
         return 1;
@@ -42,25 +50,35 @@ int verificaPrimo(int num){
 }
 
 float somaNumeros(int num){
-    float soma;
+    float soma, n;
 
+    //loop for ate o num especificado
     for(int i=0; i < num; i++){
+        //lendo o valor a ser somado
         printf("digite o numero\n> ");
         scanf("%f", &n);
+        
+        //valor incrementado a soma
         soma += n;        
     }
 
+    //mostrando a soma e retornando-a
     printf("A soma e :  %f", soma);
     return soma;
 }
 
 int main(){
     int escolha;
+
+    //escolha do usuario
     printf("O que deseja fazer?\n1 - calcular baskara\n2 - verificar se um numero e primo\n3 - somar N numeros\n> ");
     scanf("%d", &escolha);
 
+    //menu dada a opçao escolhida
     switch(escolha)
     {
+    
+    //caso opçao 1 escolhida
     case 1:
         float a, b, c;
 
@@ -76,6 +94,7 @@ int main(){
         calculaBaskara(a, b, c);
         break;
 
+    //caso opçao 2 escolhida
     case 2:
         int num;
         printf("digite o numero\n> ");
@@ -83,6 +102,7 @@ int main(){
 
         verificaPrimo(num);
 
+    //caso opçao 3 escolhida
     case 3:
         int qtd;
 
@@ -90,6 +110,7 @@ int main(){
         scanf("%d", &qtd);
         somaNumeros(qtd);
     
+    //caso nenhuma das anteriores
     default:
         printf("nenhuma opçao valida escolhida!!");
         break;
