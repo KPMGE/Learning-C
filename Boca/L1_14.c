26fee02a40329478d4971b7a3a5a4c201e425ce4
KPMGE <jesuskevin254@gmail.com>
2020-10-01 21:14:23 -0300

    Updating program

--- Boca/L1_14.c
+++ Boca/L1_14.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main(void)
+{
     //declaracao de variaveis
     int pessoas, itens, itens2;
 
@@ -9,23 +10,24 @@ int main(){
     scanf("%d%d", &pessoas, &itens);
 
     //se o numero de pessoas for superior ao de itens, logo a ultima pessoa a receber sera a que pegar o ultimo item
-    if(pessoas > itens){
+    if(pessoas > itens)
         printf("RESP:%d", itens);
-    }
 
-    else if(pessoas == itens){
+    //se o numero de pessoas igual ao de itens
+    else if(pessoas == itens)
         printf("RESP:%d", itens);
-    }
 
     //se o numero de itens for superior ao de pessoas
-    else{
-        if(itens % pessoas == 0){
+    else
+    {
+        if(itens % pessoas == 0)
             printf("RESP:%d", pessoas);
-        }
-        else{
+
+        else
+        {
             itens2 = itens % pessoas;
             printf("RESP:%d", itens2);
-         }
+        }
     }
 
 	return 0;
