7aa6d0811dc0ec57e57e9f18de15a2b80f58b579
KPMGE <jesuskevin254@gmail.com>
2020-10-05 08:01:52 -0300

    Updating program L2_18 - Boca

--- Boca/L2_18.c
+++ Boca/L2_18.c
@@ -17,80 +17,32 @@ respectivamente. Depois, L linhas com C valores V (-32767 <= V <=
 Saída: o maior valor V da matriz, seguido
 */
 
-//importação de bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
 
-//função que retorna um vetor com o valor do maior elemento e os valores da linha e coluna, respectivamente
-int* maior_valor(int **matriz, int l, int c);
-
-//função principal
 int main(void)
 {
-    //declaração de variaveis
-    int l, c, i, j, *maior;
+  int row, column;
+  int value, greatestValue = -32767, greatestRow, greatestColumn;
 
-    //leitura das dimensões da matriz
-    scanf(" %d %d", &l, &c);
+  scanf("%d %d", &row, &column);
 
-    //variável para conter o vetor de ponteiros
-    int **matriz;
+  int i, j;
+  for(i = 1; i <= row; i++)
+  {
+    for(j = 1; j <= column; j++)
+    {
+      scanf("%d", &value);
+      if(value > greatestValue)
+      {
+        greatestValue = value;
+        greatestRow = i;
+        greatestColumn = j;
+      }
+    }
+  }
 
-    //alocando matriz dinamicamente
-    matriz = (int **) malloc(l * sizeof(int *));
-    for(i = 0; i < l; i++)
-        matriz[i] = (int *) malloc(c * sizeof(int));
+  printf("%d (%d, %d)", greatestValue, greatestRow, greatestColumn);
 
-    //lendo matriz
-    for(i=0; i < l; i++)
-        for(j=0; j < c; j++)
-            //leitura do elemento da matriz na posição i, j
-            scanf(" %d", &matriz[i][j]);
-            
-    //encontrando o maior valor
-    maior = maior_valor(matriz, l, c);
-
-    //mostrando resultados
-    printf("%d (%d, %d)", maior[0], maior[1], maior[2]);
-    
-    //liberando memória alocada em cada vetor
-    for(i = 0; i < l; i++)
-        free(matriz[i]);
-
-    //liberando vetor de ponteiros e vetor para maior
-    free(matriz);
-    free(maior);
-    
-    return 0;
-}
-
-int* maior_valor(int **matriz, int l, int c)
-{
-    //declaração de variáveis
-    int i, j;
-
-    //ponteiro para o vetor que armazena o maior valor e suas coordenadas
-    int *maior;
-
-    //alocando vetor dinâmicamente
-    maior = (int *) malloc(3 * sizeof(int));
-
-    //inicializando com o valor 0 na primeira posição
-    maior[0] = 0;
-
-    //percorrendo matriz
-    for(i = 0; i < l; i++)
-        for(j = 0; j < c; j++)
-        {
-            //capturando o endereço do maior elemento
-            if(matriz[i][j] > maior[0])
-            {
-                maior[0] = matriz[i][j];
-                maior[1] = i + 1;
-                maior[2] = j + 1;
-            }
-        }
-
-    //retornando maior elemento
-    return maior;
+  return 0;
 }
