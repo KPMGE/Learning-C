403471d5a60054b3980a32d2b21efc8c02f7a6a1
KPMGE <jesuskevin254@gmail.com>
2020-10-24 09:27:20 -0300

    Updating exercise L3_7 - BOCA

--- Boca/L3_7.c
+++ Boca/L3_7.c
@@ -1,42 +1,39 @@
-#include <stdio.h>
-#include <math.h>
+// Exercise: L3_7
+// Author: Kevin Carvalho De Jesus
 
-int area(int x1, int y1, int x2, int y2);
-int area_total(int r1_x1, int r1_y1, int r1_x2, int r1_y2, int r2_x1, int r2_y1, int r2_x2, int r2_y2);
+#include <stdio.h>
+#include <stdlib.h>
+
+int calculateTotalArea(int r1_x1, int r1_y1, int r1_x2, int r1_y2, int r2_x1, int r2_y1, int r2_x2, int r2_y2);
+int calculateArea(int x1, int y1, int x2, int y2);
 
 int main(void)
 {
-    int areaFinal;
-    int r1_x1, r1_y1, r1_x2, r1_y2, r2_x1, r2_y1, r2_x2, r2_y2;
+  int r1_x1, r1_y1, r1_x2, r1_y2, r2_x1, r2_y1, r2_x2, r2_y2;
+  int totalArea;
 
-    scanf("%d %d %d %d", &r1_x1, &r1_y1, &r1_x2, &r1_y2);
-    scanf("%d %d %d %d", &r2_x1, &r2_y1, &r2_x2, &r2_y2);
+  scanf("%d%d%d%d", &r1_x1, &r1_y1, &r1_x2, &r1_y2);
+  scanf("%d%d%d%d", &r2_x1, &r2_y1, &r2_x2, &r2_y2);
 
-    areaFinal = area_total(r1_x1, r1_y1, r1_x2, r1_y2, r2_x1, r2_y1, r2_x2, r2_y2);
-
-    printf("RESP:%d", areaFinal);
-
-    return 0;
+  totalArea = calculateTotalArea(r1_x1, r1_y1, r1_x2, r1_y2, r2_x1, r2_y1, r2_x2, r2_y2);
+  printf("RESP:%d", totalArea);
+  return 0;
 }
 
-int area(int x1, int y1, int x2, int y2)
+int calculateTotalArea(int r1_x1, int r1_y1, int r1_x2, int r1_y2, int r2_x1, int r2_y1, int r2_x2, int r2_y2)
 {
-    int aresta1, aresta2, areaRetangulo;
+  int area1, area2, area3 = 0, totalArea;
 
-    aresta1 = y2 - y1;
-    aresta2 = x2 - x1;
-    areaRetangulo = aresta1 * aresta2;
+  area1 = calculateArea(r1_x1, r1_y1, r1_x2, r1_y2);
+  area2 = calculateArea(r2_x1, r2_y1, r2_x2, r2_y2);
 
-    return areaRetangulo;
+  if (r2_x1 < r2_x2 && r2_y1 < r1_y2)
+    area3 = calculateArea(r2_x1, r2_y1, r1_x2, r1_y2);
+  totalArea = area1 + area2 - area3;
+  return totalArea;
 }
 
-int area_total(int r1_x1, int r1_y1, int r1_x2, int r1_y2, int r2_x1, int r2_y1, int r2_x2, int r2_y2)
+int calculateArea(int x1, int y1, int x2, int y2)
 {
-    float area1, area2, total;
-
-    area1 = area(r1_x1, r1_y1, r1_x2, r1_y2);
-    area2 = area(r2_x1, r2_y1, r2_x2, r2_y2);
-    total = area1 + area2;
-
-    return total;
+  return (x2 - x1) * (y2 - y1);
 }
