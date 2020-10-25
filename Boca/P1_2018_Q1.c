1ce64ac2e2bbcb7cd954f3a458c722c9fc482976
KPMGE <jesuskevin254@gmail.com>
2020-10-25 17:29:19 -0300

    Adding exercise P1_2018_Q1 - BOCA

--- Boca/P1_2018_Q1.c
+++ Boca/P1_2018_Q1.c
@@ -1,25 +1,34 @@
-/*
-(BOCA: P1_2018_Q1)
-Problema: Faça um programa para dizer os quatro pontos representando os cantos de um retângulo em um plano cartesiano. O retângulo será inicialmente descrito por dois pontos representando cantos opostos.
-
-• Entrada: 4 números inteiros representando dois pontos de cantos opostos de um retângulo. Os dois primeiros números representam as coordenadas x e y do primeiro ponto e os dois últimos as coordenadas x e y do segundo ponto. Os números estarão separados por um espaço.
-
-• Saída: A saída deverá imprimir os quatro pontos do retângulo dado na entrada seguindo a ordem: canto superior esquerdo, canto inferior esquerdo, canto inferior direito e canto superior direito. Cada ponto deverá ser dado entre parênteses seguindo o formato dos exemplos abaixo
-*/
+// Exercise: P1_2018_Q1
+// Author: Kevin Carvalho de Jesus
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+typedef struct point
 {
+  int x;
+  int y;
+} Point;
 
-    int x1, y1, x2, y2;
+void displayRectangle(Point P, Point Q);
 
-    //OBS: p1=(x1, y1), p2=(x2, y2), p3=()
+int main(void)
+{
+  Point P, Q;
+  scanf("%d %d %d %d", &P.x, &P.y, &Q.x, &Q.y);
+  displayRectangle(P, Q);
 
-    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+  return 0;
+}
 
-    
-
-
-    return 0;
+void displayRectangle(Point P, Point Q)
+{
+  if((P.x == P.y && Q.x == Q.y) && P.x < Q.x)
+    printf("(%d,%d)(%d,%d)(%d,%d)(%d,%d)\n", P.x, Q.y, P.x, P.y, Q.x, P.y, Q.x, Q.y);
+  else if(P.x < Q.x && P.y > Q.y)
+    printf("(%d,%d)(%d,%d)(%d,%d)(%d,%d)\n", P.x, P.y, P.x, Q.y, Q.x, Q.y, Q.x, P.y);
+  else if(P.x > Q.x && P.y < Q.y)
+    printf("(%d,%d)(%d,%d)(%d,%d)(%d,%d)\n", Q.x, P.x, Q.x, P.y, P.x, Q.x, P.x, Q.y);
+  else 
+    printf("(%d,%d)(%d,%d)(%d,%d)(%d,%d)\n", Q.y, P.y, Q.x, Q.x, P.x, Q.y, P.x, P.y);
 }
