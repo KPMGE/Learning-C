2ed6d7676c138c967f0c6f5e08e593ac9791cbe4
KPMGE <jesuskevin254@gmail.com>
2020-11-03 15:23:14 -0300

    Updating program

--- Boca/L2_3.c
+++ Boca/L2_3.c
@@ -1,10 +1,10 @@
+// Exercicio: L2_3
+// Autor: Kevin Carvalho de jesus
+
 #include <stdio.h>
 #include <stdlib.h>
 
-/*(BOCA:L2_3) Problema: Faça um programa que leia n números positivos e retorne o maior e o menor destes,
- a quantidades de pares, a quantidade de ímpares, e a média deles.*/
-
-int main()
+int main(void)
 {
   int n, qtd, c = 0, maior = 0, menor = 0, cont = 0, pares = 0, impares = 0;
   float media = 0, mediaf;
@@ -16,27 +16,19 @@ int main()
     scanf("%d", &n);
 
     if (c == 0)
-    {
       menor = n;
-    }
 
     if (n > maior)
-    {
       maior = n;
-    }
+
     if (n < menor)
-    {
       menor = n;
-    }
 
     if (n % 2 == 0)
-    {
       pares++;
-    }
+
     else
-    {
       impares++;
-    }
 
     media += n;
     cont++;
