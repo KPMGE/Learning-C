c8d1e80a933709d83ebc714a38c9b3583af05aef
KPMGE <jesuskevin254@gmail.com>
2020-11-03 15:22:26 -0300

    Updating program

--- Boca/L1_13.c
+++ Boca/L1_13.c
@@ -1,32 +1,35 @@
+// Exercise: L3_13
+// Author: Kevin Carvalho de Jesus
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    int pessoas, itens, itens2;
+  int pessoas, itens, itens2;
 
-    scanf("%d %d", &pessoas, &itens);
+  scanf("%d %d", &pessoas, &itens);
 
-    //se o numero de pessoas for superior ao de itens, logo a ultima pessoa a receber sera a que pegar o ultimo item
-    if(pessoas > itens)
-        printf("RESP:%d", itens);
+  //se o numero de pessoas for superior ao de itens, logo a ultima pessoa a receber sera a que pegar o ultimo item
+  if (pessoas > itens)
+    printf("RESP:%d", itens);
 
-    else if(pessoas == itens)
-        printf("RESP:%d", itens);
+  else if (pessoas == itens)
+    printf("RESP:%d", itens);
 
-    //se o numero de itens for superior ao de pessoas
+  //se o numero de itens for superior ao de pessoas
+  else
+  {
+    //se o numero de itens for divisível pelo de pessoas
+    if (itens % pessoas == 0)
+      printf("RESP:%d", pessoas);
+
     else
     {
-        //se o numero de itens for divisível pelo de pessoas
-        if(itens % pessoas == 0)
-            printf("RESP:%d", pessoas);
-
-        else
-        {
-            itens2 = itens % pessoas;
-            printf("RESP:%d", itens2);
-        }
+      itens2 = itens % pessoas;
+      printf("RESP:%d", itens2);
     }
+  }
 
-	return 0;
+  return 0;
 }
