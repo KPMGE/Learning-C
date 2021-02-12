c9efe2045385f5cf23e3d31bf40b3cee676a2dbc
KPMGE <jesuskevin254@gmail.com>
2021-02-12 07:30:15 -0300

    Adding leibniz method to calculate pi

---
+++ calculating-pi/calculate-pi-leibniz.c
@@ -0,0 +1,29 @@
+// calculating the value of pi with a serie
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double calculateSerie(int size);
+
+int main(void) {
+  int size;
+
+  printf("Enter size to serie: ");
+  scanf("%d", &size);
+
+  double pi = 4 * calculateSerie(size);
+
+  printf("The value of pi is: %lf", pi);
+
+  return 0;
+}
+
+double calculateSerie(int size) {
+  double sumSerie = 0;
+
+  for (int k = 1; k <= size; k++) {
+    sumSerie += pow(-1, k + 1) / (2 * k - 1);
+  }
+  return sumSerie;
+}
