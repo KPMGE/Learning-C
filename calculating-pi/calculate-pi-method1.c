1aa8f14e60175ef53c91daaf89a66e0b7179c4c2
KPMGE <jesuskevin254@gmail.com>
2021-02-11 21:43:37 -0300

    Adding method to calculate pi

---
+++ calculating-pi/calculate-pi-method1.c
@@ -0,0 +1,29 @@
+// calculating the value of pi with a serie
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double calculateSerie(int size);
+
+int main(void) {
+  int size;
+
+  printf("Enter a size to serie: ");
+  scanf("%d", &size);
+
+  double pi = sqrt(6 * calculateSerie(size));
+
+  printf("the value of pi is: %f", pi);
+
+  return 0;
+}
+
+double calculateSerie(int size) {
+  double sumSerie = 0;
+
+  for (int i = 1; i <= size; i++) {
+    sumSerie += (1 / pow(i, 2));
+  }
+  return sumSerie;
+}
