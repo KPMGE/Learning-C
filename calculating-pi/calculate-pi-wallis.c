61f36185ff74aa288ff566bfdb37903321f970e0
KPMGE <jesuskevin254@gmail.com>
2021-02-13 14:47:12 -0300

    Adding wallis method

---
+++ calculating-pi/calculate-pi-wallis.c
@@ -0,0 +1,30 @@
+// calculating the value of pi with a serie
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double calculateSerie(int size);
+
+int main(void) {
+  int size;
+
+  printf("Enter size to serie: ");
+  scanf("%d", &size);
+
+  double pi = 2 * calculateSerie(size);
+
+  printf("The value of pi is: %lf", pi);
+
+  return 0;
+}
+
+double calculateSerie(int size) {
+  double sumSerie = 1;
+
+  for (int k = 2; k < size; k++) {
+    sumSerie *= (k / (double)(k - 1)) * (k / (double)(k + 1));
+  }
+
+  return sumSerie;
+}
