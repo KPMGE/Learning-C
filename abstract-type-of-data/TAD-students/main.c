6ff768a912b361d73d2e0e932839fed61df9e1e8
KPMGE <jesuskevin254@gmail.com>
2021-02-18 17:13:01 -0300

    Adding main program for students

---
+++ abstract-type-of-data/TAD-students/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "student.h"
+
+int main(void) {
+  // creating a student
+  Student *student1 = createStudent();
+
+  // reading student's data
+  readStudent(student1);
+  printf("\n");
+
+  // displaying results
+  displayStudent(student1);
+  printf("student average: %.2f", calculateAveragestudent(student1));
+  
+  // releasing memory
+  freeStudent(student1);
+
+  return 0;
+}
