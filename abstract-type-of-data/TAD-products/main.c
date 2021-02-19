a7e1fa45304eb0f393d9a6ab19e6fc41f15f517e
KPMGE <jesuskevin254@gmail.com>
2021-02-18 23:02:08 -0300

    Adding main program

---
+++ abstract-type-of-data/TAD-products/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "product.h" 
+#include "list-products.h"
+
+float readExpenses();
+ 
+int main(void) {
+  // clear screen
+  system("clear");
+
+  char choiceUser;
+  int amountProducts;
+  float totalExpenses, bruteValue;
+
+  printf("how many products do you want to add? ");
+  scanf("%d", &amountProducts);
+
+  // creating allocated list
+  Product *listProducts = createListProducts(amountProducts);
+
+  // reading list
+  readListProducts(listProducts, amountProducts);
+
+  // reading expenses
+  totalExpenses = readExpenses();
+  bruteValue = getBruteValue(listProducts, amountProducts);
+
+  // clear screen
+  system("clear");
+
+  // displaying results
+  printf("\nThe brute value is: R$%.2f\n", bruteValue);
+  printf("Your expenses were: R$%.2f\n", totalExpenses);
+  printf("Your gain was: %.2f\n\n", bruteValue - totalExpenses);
+
+  printf("do you want to see all your list: (Y/N) ");
+  scanf(" %c", &choiceUser);
+
+  if (choiceUser == 'y' || choiceUser == 'Y') {
+    displayListProducts(listProducts, amountProducts);
+  }
+
+  // releasing allocated memory
+  freeListProducts(listProducts);
+
+  return 0;
+}
+
+float readExpenses() {
+  float sum = 0, value;
+  printf("Enter your expenses:\n(Enter 0 to stop)\n");
+
+  do {
+    scanf("%f", &value);
+    sum += value;
+  } while(value != 0);
+
+  return sum;
+}
