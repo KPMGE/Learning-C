190f69f0733e5a02438f8fc5961a53ac72731c46
KPMGE <jesuskevin254@gmail.com>
2021-02-22 15:37:19 -0300

    Adding header for point type

---
+++ tic-tac-toe-game/include/point.h
@@ -0,0 +1,14 @@
+// type point for game
+
+// exported structure
+typedef struct point Point;
+// function that sets values to a point
+void setPoint(Point *point, float x, float y);
+// function that returns a point in a x-y coordinate
+void getPoint(Point *point, float *x, float *y);
+// function that displays a pointt
+void displayPoint(Point *point);
+// function that releases a point
+void freePoint(Point *point);
+// function that creates new instances of a point
+Point* createPoint(float x, float y);
