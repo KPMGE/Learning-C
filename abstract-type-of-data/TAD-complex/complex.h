690773241b2e503097939bdf4c2f030b884e1d8b
KPMGE <jesuskevin254@gmail.com>
2021-03-02 22:50:45 -0300

    Adding first version of complex.h

---
+++ abstract-type-of-data/TAD-complex/complex.h
@@ -0,0 +1,22 @@
+// exported type
+typedef struct complex D_Complex_t;
+
+//  functions that creates a complex number
+D_Complex_t* createComplex(double real, double imag);
+// function that sums two complex numbers
+D_Complex_t* sumComplex(D_Complex_t *number1, D_Complex_t *number2);
+// function that subtracts two complex numbers
+D_Complex_t* subtractComplex(D_Complex_t *number1, D_Complex_t *number2);
+// function that subtracts number1 and number2 and assign outcome to number3
+D_Complex_t* subractComplexInto(D_Complex_t *number1, D_Complex_t *number2, D_Complex_t *number3);
+// function that returns real field of a complex number
+double getRealField(D_Complex_t *number);
+// function that returns imaginary field of a complex number
+double getImaginaryField(D_Complex_t *number);
+// function that accumulates the sum of number1 and number2 in number1
+void accumulateComplex(D_Complex_t **number1, D_Complex_t *number2);
+// function that displays the complex number
+void displayComplex(D_Complex_t *number);
+// function that releases an allocated complex number
+void freeComplex(D_Complex_t *number);
+
