0d7d1cbbb0ebbff7348c3e80f621e7eafb9a7b0a
KPMGE <jesuskevin254@gmail.com>
2021-03-04 07:23:42 -0300

    Updating main program

--- abstract-type-of-data/TAD-complex/main.c
+++ abstract-type-of-data/TAD-complex/main.c
@@ -3,23 +3,41 @@
 #include "complex.h"
 
 int main(void) {
-  D_Complex_t *number1, *number2, *sumNumbers;
+  D_Complex_t *number1, *number2, *sumNumbers, *subtractionNumbers, *multiNumbers, *divisionNumbers;
 
   number1 = createComplex(1, 4);
   number2 = createComplex(2, 3);
 
+  // doing operations
   sumNumbers = sumComplex(number1, number2);
-
-  printf("sum: \n");
+  subtractionNumbers = subtractComplex(number1, number2);
+  multiNumbers = multiplicateComplex(number1, number2);
+  divisionNumbers = divisionComplex(number1, number2);
+  
+  // displaying results
+  printf("num1 + num2: ");
   displayComplex(sumNumbers);
 
-  printf("\n\naccumulated num1: \n");
+  printf("\n\nnum1 - num2: ");
+  displayComplex(subtractionNumbers);
+
+  printf("\n\nnum1 * num2: ");
+  displayComplex(multiNumbers);
+
+  printf("\n\nnum1 / num2: ");
+  displayComplex(divisionNumbers);
+
+  printf("\n\naccumulated num1: ");
   accumulateComplex(&number1, number2);
   displayComplex(number1);
 
+  // freeing  memory
   freeComplex(number1);
   freeComplex(number2);
   freeComplex(sumNumbers);
+  free(subtractionNumbers);
+  free(divisionNumbers);
+  free(multiNumbers);
 
   return 0;
 }
