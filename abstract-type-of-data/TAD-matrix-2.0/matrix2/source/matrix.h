a132e9b9845c2db2c8f79d422eff19789e5a88f0
KPMGE <jesuskevin254@gmail.com>
2021-03-17 10:59:12 -0300

    Adding header

---
+++ abstract-type-of-data/TAD-matrix-2.0/matrix2/source/matrix.h
@@ -0,0 +1,19 @@
+#ifndef MATRIZ_H_INCLUDED
+#define MATRIZ_H_INCLUDED
+
+#define TYPE float 
+#define TYPED(SOMETHING) F_ ## SOMETHING
+
+typedef struct matrix TYPED(Matrix_t);
+
+TYPED(Matrix_t*) TYPED(createMatrix) (unsigned int rows, unsigned int cols);
+TYPE TYPED(getValueMatrix) (TYPED(Matrix_t *) matrix, unsigned int row, unsigned int col);
+TYPE TYPED(getAmountRows) (TYPED(Matrix_t *) matrix);
+TYPE TYPED(getAmountColumns) (TYPED(Matrix_t *) matrix);
+void TYPED(setValueToMatrix) (TYPED(Matrix_t *) matrix, unsigned int row, unsigned int col, TYPE value);
+void TYPED(fillMatrix) (TYPED(Matrix_t *) matrix, TYPE value);
+void TYPED(displayMatrix) (TYPED(Matrix_t *) matrix);
+TYPED(Matrix_t*) TYPED(multiplicateMatrix) (TYPED(Matrix_t *) matrix1, TYPED(Matrix_t *) matrix2);
+void TYPED(freeMatrix) (TYPED(Matrix_t *) matrix);
+
+#endif
